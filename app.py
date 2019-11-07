251c241892ba75ca25368544af605e9f0dca8ccb
tirso <tirso.cordova.briz@gmail.com>
2019-11-06 18:40:11 -0600

    fixed tons of stuff

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify, render_template, request
 import os, optparse
+import json
 
 app = Flask(__name__)
-
+app.static_folder = 'templates'
 developer = os.getenv("DEVELOPER", "Me")
 environment = os.getenv("ENVIRONMENT", "development")
+
+
+@app.route('/')
+def hello_world():
+    return render_template('cards.html')
+
 
 if __name__ == "__main__":
     debug = False
